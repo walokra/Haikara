69ba23493fedea0aa4d626997c12bf3fcd5c0ed5
Marko Wallin <marko.wallin@iki.fi>
2015-06-14 22:18:01 +0300

    Initial Commit

---
+++ Haikara/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Haikara
+//
+//  Created by Marko Wallin on 14.6.2015.
+//  Copyright (c) 2015 Rule of tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
