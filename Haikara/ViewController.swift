0c0296d133836211feea2300d76ea43d1536a0c6
Marko Wallin <marko.wallin@iki.fi>
2015-06-20 20:51:19 +0300

    Map JSON directly to class. Can use only iOS 8 or newer target with embedded lib. Show 2 lines of description.

--- Haikara/ViewController.swift
+++ Haikara/ViewController.swift
@@ -39,10 +39,10 @@ import Alamofire
 //},
 
 class NewsEntry: NSObject {
-	var title: String = ""
-	var link: String = ""
-	var author: String = ""
-	var publishedDateJS: String = ""
+	let title: String
+	let link: String
+	let author: String
+	let publishedDateJS: String
 	//	let picture: String?
 	//	let originalPicture: String?
 	var shortDescription: String?
@@ -50,27 +50,28 @@ class NewsEntry: NSObject {
 	var mobileLink: String?
 	//	let originalMobileUrl: String?
 	//	let articleID: Int
-	var sectionID: Int = 0
+	var sectionID: Int
 	//	let sourceID: Int
 	//	let highlight: Bool
+	var since: String?
 	
-//	init(title: String, link: String, author: String, publishedDateJS: String,
-//		shortDescription: String?, mobileLink: String?, sectionID: Int) {
-//		self.title = title
-//		self.link = link
-//		self.author = author
-//		self.publishedDateJS = publishedDateJS
-//		//	let picture: String?
-//		//	let originalPicture: String?
-//		self.shortDescription = shortDescription
-//		//	let originalURL: String
-//		self.mobileLink = mobileLink
-//		//	let originalMobileUrl: String?
-//		//	let articleID: Int
-//		self.sectionID = sectionID
-//		//	let sourceID: Int
-//		//	let highlight: Bool
-//	}
+	init(title: String, link: String, author: String, publishedDateJS: String,
+		shortDescription: String?, mobileLink: String?, sectionID: Int) {
+		self.title = title
+		self.link = link
+		self.author = author
+		self.publishedDateJS = publishedDateJS
+		//	let picture: String?
+		//	let originalPicture: String?
+		self.shortDescription = shortDescription
+		//	letoriginalURL: String
+		self.mobileLink = mobileLink
+		//	let originalMobileUrl: String?
+		//	let articleID: Int
+		self.sectionID = sectionID
+		//	let sourceID: Int
+		//	let highlight: Bool
+	}
 	
 	override var description: String {
 		return "newsEntry: title=\(self.title), link=\(self.link), author=\(self.author), published=\(self.publishedDateJS), desc=\(self.shortDescription), mobileLink=\(self.mobileLink), sectionID=\(self.sectionID)"
@@ -81,39 +82,53 @@ class NewsEntry: NSObject {
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
 	let cellIdentifier = "tableCell"
-	//var newsEntries = NSMutableOrderedSet()
-	var newsEntries: [NewsEntry] = []
+	var newsEntries = NSMutableOrderedSet()
 	let APIKEY: String = ""
 
-	@IBOutlet weak var tableView: UITableView?
+	@IBOutlet weak var tableView: UITableView!
 
 	// MARK: Lifecycle
 	
     override func viewDidLoad() {
         super.viewDidLoad()
 		
-		self.tableView!.dataSource = self
+		self.navigationItem.title = "Uutiset";
 
+		self.tableView!.dataSource = self
+		
         getHighFiJSON("http://fi.high.fi/news/json-private")
 		
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
+
     }
+	
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		NSNotificationCenter.defaultCenter().addObserver(self, selector: "contentSizeCategoryChanged:", name: UIContentSizeCategoryDidChangeNotification, object: nil)
+	}
+	
+	override func viewDidDisappear(animated: Bool) {
+		super.viewDidDisappear(animated)
+		
+		NSNotificationCenter.defaultCenter().removeObserver(self, name: UIContentSizeCategoryDidChangeNotification, object: nil)
+	}
 	
 	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 		if segue.identifier == "NewsItemDetails" {
 			let path = self.tableView!.indexPathForSelectedRow()!
 			let row = path.row
-
-			let highfiEntry : NewsEntry = self.newsEntries[row] as NewsEntry
-			(segue.destinationViewController as! NewsItemViewController).webSite = highfiEntry.link
-
-//			(segue.destinationViewController as! NewsItemViewController).webSite = (newsEntries.objectAtIndex(row) as! NewsEntry).link
 			
-//			let highfiEntry : NSMutableDictionary = self.newsEntries.objectAtIndex(row) as! NSMutableDictionary
-//			if let link = highfiEntry["link"] as? String {
-//				(segue.destinationViewController as! NewsItemViewController).webSite = link
-//			}
+			let highfiEntry : NewsEntry = self.newsEntries.objectAtIndex(row) as! NewsEntry
+//			println("mobileLink= \(highfiEntry.mobileLink)")
+//			println("link= \(highfiEntry.link)")
+			(segue.destinationViewController as! NewsItemViewController).title = highfiEntry.title
+			if (highfiEntry.mobileLink?.isEmpty != nil) {
+				(segue.destinationViewController as! NewsItemViewController).webSite = highfiEntry.link
+			} else {
+				(segue.destinationViewController as! NewsItemViewController).webSite = highfiEntry.mobileLink
+			}
 		}
 	}
 
@@ -132,14 +147,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
      func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		let cell: UITableViewCell! = tableView.dequeueReusableCellWithIdentifier(self.cellIdentifier, forIndexPath: indexPath) as! UITableViewCell
 		
-		let highfiEntry : NewsEntry = self.newsEntries[indexPath.row] as NewsEntry
-//		println("highfiEntry: \(highfiEntry)")
-		
-		cell.textLabel!.text = highfiEntry.title
-		cell.detailTextLabel?.text = highfiEntry.shortDescription
-
-//		cell.textLabel!.text = (newsEntries.objectAtIndex(indexPath.row) as! NewsEntry).title
-//		cell.detailTextLabel?.text = (newsEntries.objectAtIndex(indexPath.row) as! NewsEntry).shortDescription
+		// Configure the cell for this indexPath
+		cell.textLabel!.text = (newsEntries.objectAtIndex(indexPath.row) as! NewsEntry).title
+		cell.detailTextLabel?.text = (newsEntries.objectAtIndex(indexPath.row) as! NewsEntry).shortDescription
+		//cell.detailTextLabel!.lineBreakMode = .ByWordWrapping;
+		cell.detailTextLabel!.numberOfLines = 2; // Show 2 lines
 
 		return cell
     }
@@ -155,59 +167,31 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 				
 			if error == nil {
 				dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)) {
-					//let entries : NSDictionary = (theJSON as! NSDictionary).valueForKey("entries") as! NSDictionary
-
 					let data = (JSON!.valueForKey("responseData") as! NSDictionary)
 					let feed = (data.valueForKey("feed") as! NSDictionary)
 					let entries = (feed.valueForKey("entries") as! [NSDictionary])
-					//println("entries: \(entries.count)")
-					
-					for item in entries {
-						var entry: NewsEntry = NewsEntry()
-
-						entry.title = item.valueForKey("title") as! String
-						entry.link = item.valueForKey("link") as! String
-						entry.author = item.valueForKey("author") as! String
-						entry.publishedDateJS = item.valueForKey("publishedDateJS") as! String
-						if let desc = item.valueForKey("shortDescription") as? String {
-							entry.shortDescription = desc
-						}
-						if let mobileLink = item.valueForKey("mobileLink") as? String {
-							entry.mobileLink = mobileLink
-						}
-						entry.sectionID = item.valueForKey("sectionID") as! Int
-						self.newsEntries.append(entry)
+						//.filter({ ($0["sectionID"] as! Int) == 98 })
+						.map { NewsEntry(
+							title: $0["title"] as! String,
+							link: $0["link"] as! String,
+							author: $0["author"] as! String,
+							publishedDateJS: $0["publishedDateJS"] as! String,
+							shortDescription: $0["shortDescription"] as? String,
+							mobileLink: $0["mobileLink"] as? String,
+							sectionID: $0["sectionID"] as! Int
+							//	let picture: String?
+							//	let originalPicture: String?
+							//	let originalURL: String
+							//	let originalMobileUrl: String?
+							//	let articleID: Int
+							//	let sourceID: Int
+							//	let highlight: Bool
+						)
 					}
-					
-					println("newsEntries: \(self.newsEntries.count)")
-
-//					let response : NSDictionary = (JSON as! NSDictionary).valueForKey("responseData") as! NSDictionary
-//					let feed : NSDictionary = response["feed"] as! NSDictionary
-//					// println("feed: \(feed)")
-//					let results : NSArray = feed["entries"] as! NSArray
-					
-//					let entries = ((JSON as! NSDictionary).valueForKey("entries") as! [NSDictionary])
-//						//.filter({ ($0["sectionID"] as! Int) == 98 })
-//					.map { NewsEntry(
-//							title: $0["title"] as! String,
-//							link: $0["link"] as! String,
-//							author: $0["author"] as! String,
-//							publishedDateJS: $0["publishedDateJS"] as! String,
-//							shortDescription: $0["shortDescription"] as? String,
-//							mobileLink: $0["mobileLink"] as? String,
-//							sectionID: $0["sectionID"] as! Int
-//						)
-//						//	let picture: String?
-//						//	let originalPicture: String?
-//						//	let originalURL: String
-//						//	let originalMobileUrl: String?
-//						//	let articleID: Int
-//						//	let sourceID: Int
-//						//	let highlight: Bool
-//					}
+					println("entries: \(entries.count)")
 					
 					dispatch_async(dispatch_get_main_queue()) {
-						//self.newsEntries.addObjectsFromArray(entries)
+						self.newsEntries.addObjectsFromArray(entries)
 						self.tableView!.reloadData()
 
 						return
@@ -218,7 +202,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 			}
 		}
 	}
-
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
