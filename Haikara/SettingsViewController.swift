6be9a3478c49104caa70341e46d0e2529be24f4f
Marko Wallin <marko.wallin@iki.fi>
2015-09-10 23:55:01 +0300

    Fix Settings with ScrollView

--- Haikara/SettingsViewController.swift
+++ Haikara/SettingsViewController.swift
@@ -50,6 +50,24 @@ class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         countryPicker.dataSource = self
         countryPicker.delegate = self
     }
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    
+    override func viewDidLayoutSubviews() {
+        let scrollViewBounds = scrollView.bounds
+        let containerViewBounds = contentView.bounds
+        
+        var scrollViewInsets = UIEdgeInsetsZero
+        scrollViewInsets.top = scrollViewBounds.size.height/2.0;
+        scrollViewInsets.top -= contentView.bounds.size.height/2.0;
+        
+        scrollViewInsets.bottom = scrollViewBounds.size.height/2.0
+        scrollViewInsets.bottom -= contentView.bounds.size.height/2.0;
+        scrollViewInsets.bottom += 1
+        
+        scrollView.contentInset = scrollViewInsets
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
