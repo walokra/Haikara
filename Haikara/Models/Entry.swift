0b14ec4b36df2f2c900924a21bbd8b97049302e9
Marko Wallin <marko.wallin@gofore.com>
2017-12-22 14:09:32 +0200

    Update license to files

--- Haikara/Models/Entry.swift
+++ Haikara/Models/Entry.swift
@@ -2,39 +2,30 @@
 //  Entry.swift
 //  highkara
 //
-//  Created by Marko Wallin on 4.11.2015.
-//  Copyright © 2015 Rule of tech. All rights reserved.
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2017 Marko Wallin <mtw@iki.fi>
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import UIKit
-
-//{
-//	"responseData": {
-//		"feed": {
-//			"title": "HIGH.FI",
-//			"link": "http://fi.high.fi/",
-//			"author": "AfterDawn Oy",
-//			"description": "News",
-//			"type": "json",
-//			"entries": [
-//{
-//	"title": "string",
-//	"link": "url",
-//	"author": "string",
-//	"publishedDateJS": "2015-06-16T22:46:08.000Z",
-//	"publishedDate": "June, 16 2015 22:46:08",
-//	"originalPicture": "url",
-//	"picture": "url",
-//	"shortDescription": "",
-//	"originalURL": "url",
-//	"mobileLink": "",
-//	"originalMobileURL": "",
-//	"articleID": int,
-//	"sectionID": int,
-//	"sourceID": int,
-//	"highlight": true
-//  "highlightType": "popular"
-//},
 
 class Entry: NSObject {
     let title: String
